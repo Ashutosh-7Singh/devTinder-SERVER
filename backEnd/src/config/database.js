2360303f636388e495ac2a62110488dc50660ea6
Ashutosh Singh <1709ashutosh@gmail.com>
2025-07-10 20:55:41 +0530

    update

--- backEnd/src/config/database.js
+++ backEnd/src/config/database.js
@@ -1,7 +1,8 @@
 const mongoose=require("mongoose");;
 
 const connectbDb=async()=>{
-    await mongoose.connect("mongodb://localhost:27017/devBaba")
+    // await mongoose.connect("mongodb://localhost:27017/devBaba")
+    await mongoose.connect("mongodb+srv://theashutosh96:MM6IMakdushnO7Xh@cluster0.oxc8t7f.mongodb.net/devTinder")
 }
 
 
